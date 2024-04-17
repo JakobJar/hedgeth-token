d0afe78bbcf58d35fe4d0fa7adac71cf35d6cb54
Jakob <jakob.stiel@gmail.com>
2024-04-17 19:42:49 +0200

    Implement HedgethToken

--- contracts/HedgethToken.sol
+++ contracts/HedgethToken.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract HedgethToken {
-    constructor(){
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+contract HedgethToken is ERC20 {
+
+    constructor() ERC20("Hedgeth Token", "HDT") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 }
